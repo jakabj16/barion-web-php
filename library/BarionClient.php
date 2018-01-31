da9e5df9d292f237d1949f2b8e0106b02be0fdd9
jakabj <jakabj16@gmail.com>
2018-01-31 13:37:24 +0100

    CURLOPT_SSL_VERIFYPEER false

--- library/BarionClient.php
+++ library/BarionClient.php
@@ -220,6 +220,7 @@ class BarionClient
 
         $postData = json_encode($data);
 
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
@@ -271,6 +272,7 @@ class BarionClient
             $userAgent = "curl/" . $cver["version"] . " " .$cver["ssl_version"];
         }
 
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_URL, $fullUrl);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array("User-Agent: $userAgent"));
